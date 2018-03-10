8d0cd274f40c29530a8a43a946cc440fe7b88b1a
sanchitgnawali <sanchitgnawali@gmail.com>
2018-03-10 15:16:04 -0600

    added test for /get totos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,19 @@ const expect = require('expect');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [
+    {
+        text: "first text object"
+    },
+    {
+        text: "second text object"
+    }
+];
+
 beforeEach((done)=>{
-    Todo.remove({}).then(()=>done());
+    Todo.remove({}).then(()=>{
+       return Todo.insertMany(todos);
+    }).then(()=>done());
 });
 
 describe('POST /todos',()=>{
@@ -24,7 +35,7 @@ describe('POST /todos',()=>{
                 return done(err)
             }
 
-            Todo.find().then((todos)=>{
+            Todo.find({text}).then((todos)=>{
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -32,10 +43,10 @@ describe('POST /todos',()=>{
         });
     });
 
-    it('should return 400 when data is invalid',()=>{
+    it('should return 400 when data is invalid',(done)=>{
         request(app)
         .post('/todos')
-        .send('hi')
+        .send({})
         .expect(400)
         .end((err,res)=>{
             if(err) {
@@ -43,11 +54,24 @@ describe('POST /todos',()=>{
             }
             
             Todo.find().then((todos) =>{
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
-            }).catch((e)=>done(e));
+            }).catch((e) => done(e));
         })
 
+    });
+
+});
+
+describe('/GET todos',() =>{
+
+    it('should get all the todos',(done)=>{
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todos.length).toBe(2);
+        }).end(done);
     });
 
 });
