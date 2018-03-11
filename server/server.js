c79157c3d58d6dfa711ca75df62e92f6da13c319
sanchitgnawali <sanchitgnawali@gmail.com>
2018-03-11 14:42:39 -0500

    added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,10 +4,12 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectId} = require('mongodb');
 
 var app = express();
 app.use(bodyParser());
 
+//make a post request sending the data
 app.post('/todos',(req,res)=>{
     var todo = new Todo({
         text: req.body.text
@@ -16,19 +18,37 @@ app.post('/todos',(req,res)=>{
         res.send(doc);
     },(err)=>{
         res.status(400).send(err);
-    })
+    });
 });
 
+//getting all the todo
 app.get('/todos',(req,res)=>{
     Todo.find().then((todos)=>{
         res.send({todos});
     },(e)=>{
         res.status(400).send(e);
     });
-})
+});
+
+//get request with a parameter
+app.get('/todos/:id',(req,res)=>{
+   
+    var id = req.params.id;
+    if(!ObjectId.isValid(id)) {
+        return res.status(404).send("Invalid id");
+    } 
+    Todo.findById(id).then((todo)=>{
+        if(!todo) {
+            return res.status(404).send();
+        }   
+        res.send(todo);
+    }).catch((e)=>{
+        res.status(400).send();
+    });
+});
 
 app.listen(3000,()=>{
     console.log('Started on port 3000');
-})
+});
 
 module.exports = {app};
