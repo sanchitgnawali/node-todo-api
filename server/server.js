6698acdeb75c06eb20dff9e80a016dee2eeab972
sanchitgnawali <sanchitgnawali@gmail.com>
2018-03-13 11:45:57 -0500

    Added DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -50,6 +50,28 @@ app.get('/todos/:id',(req,res)=>{
     });
 });
 
+// Deleting a doc
+app.delete('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+
+    if(!ObjectId.isValid(id)) {
+        return res.status(404).send("Invalid id");
+    }
+
+    Todo.findByIdAndRemove(id).then((todo)=> {
+        
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send(todo)
+
+    }).catch((e)=>{
+        res.status(400).send();
+    });    
+});
+
+
 app.listen(port,()=>{
     console.log(`Started on port ${port}`);
 });
